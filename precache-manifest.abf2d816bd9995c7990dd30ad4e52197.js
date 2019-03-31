670296a4ca241bcb3667fceb96963b394d3862c5
itsthabtlackkidd <sheku.kanneh12@gmail.com>
2019-03-31 16:43:32 +1100

    Updates

---
+++ precache-manifest.abf2d816bd9995c7990dd30ad4e52197.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "18834a883f30bd9cba080b3fa99e0a49",
+    "url": "/ItsThatBlackKid/3788996.github.io/static/media/sheku_kanneh.18834a88.jpg"
+  },
+  {
+    "revision": "192bb34751144dbbc0148dc922851cb3",
+    "url": "/ItsThatBlackKid/3788996.github.io/static/media/ideal_job.192bb347.png"
+  },
+  {
+    "revision": "a8dfb0c8a865f2f66c84",
+    "url": "/ItsThatBlackKid/3788996.github.io/static/js/runtime~main.9af69cec.js"
+  },
+  {
+    "revision": "02e0dd4f49d400d6ed0d",
+    "url": "/ItsThatBlackKid/3788996.github.io/static/js/main.1215eab8.chunk.js"
+  },
+  {
+    "revision": "2b01a1a5d1d30add86eb",
+    "url": "/ItsThatBlackKid/3788996.github.io/static/js/2.0acda083.chunk.js"
+  },
+  {
+    "revision": "02e0dd4f49d400d6ed0d",
+    "url": "/ItsThatBlackKid/3788996.github.io/static/css/main.2baae28d.chunk.css"
+  },
+  {
+    "revision": "aa10cf8e1b633227b2ed152aa54b18de",
+    "url": "/ItsThatBlackKid/3788996.github.io/index.html"
+  }
+];
