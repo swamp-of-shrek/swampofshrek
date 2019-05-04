ddd80d5fe95a8134ce5eb3e1ab85133329497188
itsthabtlackkidd <sheku.kanneh12@gmail.com>
2019-05-04 21:55:12 +1000

    pictures have been added to the profiles now

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,35 +1,44 @@
 import React, {Fragment} from "react"
 import FloatingPaper from "../components/FloatingPaper";
 import {Typography} from "@material-ui/core";
-import myerbriggs from "../static/myerbriggs.png";
-import learnStyle from "../static/learnStyle.png";
-import polComp from "../static/polComp.png";
+import sheku_kanneh from '../static/sheku_kanneh.jpg';
+import adam from '../static/adam.png'
+import nandini from '../static/nandini.png'
+import peng from '../static/peng.jpg'
 
 export default function MyProfile() {
     return (
         <Fragment>
             <FloatingPaper>
-                <Typography variant={"h4"}>
-                    <b>Peng Cheng Lin</b>
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    ID: s3664976
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Email Address: s3664976@student.rmit.edu.au
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Nationality: China
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Culture: Chinese Civilisation
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Education: Senior High School
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Languages: Chinese
-                </Typography>
+
+                <div style={{display: 'flex'}}>
+                    <img width={300} height={400} src={peng}/>
+                    <div className={"facts"} style={{padding: 10}}>
+                        <Typography variant={"h4"}>
+                            <b>Peng Cheng Lin</b>
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            ID: s3664976
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Email Address: s3664976@student.rmit.edu.au
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Nationality: China
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Culture: Chinese Civilisation
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Education: Senior High School
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Languages: Chinese
+                        </Typography>
+                    </div>
+                </div>
+
+
 
                 <br/>
                 <Typography variant={"h5"}>
@@ -49,36 +58,38 @@ export default function MyProfile() {
             </FloatingPaper>
 
             <FloatingPaper>
-                <Typography variant={"h4"}>
-                    <b>Nandini Nair</b>
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    ID: S3784955
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Email Address: s3784955@student.rmit.edu.au
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Nationality: Ethnically India, but actually from Singapore
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Education to date: RMIT University (Bach of IT)
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Languages Spoken: French, English, Hindi, Malayalam
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Passionate about: Digital art
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Interesting Fact: Whilst writing this website,
-                    I would be watching to or listening Hamilton The Musical for inspiration to do it well.
-                </Typography>
 
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Honest Confession: I don't rightly know why I made a whole extra page for just this?
-                    It seems like a waste of time?
-                </Typography>
+                <div style={{display: 'flex'}}>
+                    <img width={300} height={400} src={nandini}/>
+                    <div className={"facts"} style={{padding: 10}}>
+                        <Typography variant={"h4"}>
+                            <b>Nandini Nair</b>
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            ID: S3784955
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Email Address: s3784955@student.rmit.edu.au
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Nationality: Ethnically India, but actually from Singapore
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Education to date: RMIT University (Bach of IT)
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Languages Spoken: French, English, Hindi, Malayalam
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Passionate about: Digital art
+                        </Typography>
+
+                        <Typography variant={"subtitle2"}>
+                            I used to run a blog from May 2017 until December 2018 and posted more than 500k words of
+                            short stories there, and accumulated more than 13k people as a following.
+                        </Typography>
+                    </div>
+                </div>
 
                 <br/>
                 <Typography variant={"h5"}>
@@ -93,29 +104,41 @@ export default function MyProfile() {
             </FloatingPaper>
 
             <FloatingPaper>
-                <Typography variant={"h4"}>
-                    <b>Adam Burcher</b>
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    ID: s3769238
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Email Address: burchera@gmail.com
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Nationality: Australian
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Current Education: BA of Information Technology
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    League of Legends Match History: <a
-                    href={"https://oce.op.gg/summoner/userName=Going%20for%20Silver"}>Here</a>
-                </Typography>
-                <Typography variant={"subtitle1"} style={{marginTop: 10}}>
-                    Honest Confession: I don't rightly know why I made a whole extra page for just this? It seems like a
-                    waste of time
-                </Typography>
+
+                <div style={{display: 'flex'}}>
+                    <img width={600} height={400} src={adam}/>
+                    <div className={"facts"} style={{padding: 10}}>
+                        <Typography variant={"h4"}>
+                            <b>Adam Burcher</b>
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            ID: s3769238
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Email Address: burchera@gmail.com
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Nationality: Australian
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Current Education: BA of Information Technology
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            League of Legends Match History: <a
+                            href={"https://oce.op.gg/summoner/userName=Going%20for%20Silver"}>Here</a>
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Interesting Fact: Whilst writing this website,
+                            I would be watching to or listening Hamilton The Musical for inspiration to do it well.
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            Honest Confession: I don't rightly know why I made a whole extra page for just this? It
+                            seems like a
+                            waste of time
+                        </Typography>
+                    </div>
+                </div>
+
 
                 <br/>
                 <Typography variant={"h5"}>
@@ -163,6 +186,53 @@ export default function MyProfile() {
                     INFP
                 </Typography>
                 <Typography variant={"body1"}>
+                    Mediator personalities are true idealists, always looking for the hint of good in even the worst of
+                    people and events, searching for ways to make things better. While they may be perceived as calm,
+                    reserved, or even shy, Mediators have an inner flame and passion that can truly shine. Comprising
+                    just 4% of the population, the risk of feeling misunderstood is unfortunately high for the Mediator
+                    personality type – but when they find like-minded people to spend their time with, the harmony they
+                    feel will be a fountain of joy and inspiration.
+                </Typography>
+            </FloatingPaper>
+
+            <FloatingPaper>
+                <div style={{display: 'flex'}}>
+                    <img width={300} height={400} src={sheku_kanneh}/>
+                    <div className={"facts"} style={{padding: 10}}>
+                        <Typography variant={"h4"}>
+                            <b>Sao Sheku Kanneh</b>
+                        </Typography>
+                        <Typography variant={"subtitle1"} style={{marginTop: 10}}>
+                            ID: s3788996
+                        </Typography>
+
+                        <Typography variant={"subtitle1"}>
+                            Age: 18
+                        </Typography>
+                        <Typography variant={"subtitle1"}>
+                            Email: s3788996@student.rmit.edu.au
+                        </Typography>
+                        <Typography variant={"subtitle1"}>
+                            Nationality: Liberian
+                        </Typography>
+
+                        <Typography variant={"subtitle1"}>
+                            DOB: 10/07/2000
+                        </Typography>
+
+                        <Typography variant={"subtitle1"}>
+                            I Like: Pokemon, Super Smash, Lasagna, Kendrick Lamar
+                        </Typography>
+                    </div>
+                </div>
+                <br/>
+
+
+                <Typography variant={"h5"}>
+                    INFP-T
+                </Typography>
+
+                <Typography variant={"h5"}>
                     Mediator personalities are true idealists, always looking for the hint of good in even the worst of
                     people and events, searching for ways to make things better. While they may be perceived as calm,
                     reserved, or even shy, Mediators have an inner flame and passion that can truly shine. Comprising
